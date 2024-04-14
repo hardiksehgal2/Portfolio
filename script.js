40cccf94e594f87fa8bb77f28eed5ac513f48721
Hardik Sehgal <hardiksehgal00@gmail.com git config --global user.email hardiksehgal00@gmail.com>
2024-04-15 03:17:39 +0530

    1st commit

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+var icon=document.getElementById("moon-img");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src="assets/sun.png";
+    }
+    else{
+        icon.src="assets/moon.png"
+    }
+}
